ea278b3bf3fa5c7f5ba9e79828272e419586af70
백서영 <101791501+seoye0ng@users.noreply.github.com>
2023-04-16 21:42:09 +0900

    Feat: reset 버튼 기능 구현

--- Calculator/main.js
+++ Calculator/main.js
@@ -3,11 +3,14 @@ const output = document.querySelector('#output');
 const numBtns = document.querySelectorAll('.input-number input');
 const operatorBtns = document.querySelectorAll('.input-operator input');
 
+const optionBtn = document.querySelectorAll('.options input[type="button"]');
+
 let storage = [];
 let operator = [];
 
 clickOperatBtn(operatorBtns);
 clickNumBtn(numBtns);
+
 // 1.
 function clickNumBtn() {
   numBtns.forEach((btn) => {
@@ -16,6 +19,7 @@ function clickNumBtn() {
     });
   });
 }
+
 // 2.
 function clickOperatBtn(operatorBtns) {
   operatorBtns.forEach((btn) => {
@@ -26,3 +30,14 @@ function clickOperatBtn(operatorBtns) {
     });
   });
 }
+
+//3. reset
+optionBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (btn.value === 'RESET') {
+      output.value = '';
+      storage = [];
+      operator = [];
+    }
+  });
+});
